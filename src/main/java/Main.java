b207542d0c95c42d4565ff36a76786e1998c2d57
Maksim <stelmakh96@gmail.com>
2018-03-03 15:30:30 +0300

    3:28 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,8 @@ public class Main {
     public static void main(String[] args) {
         String abc = "Heello world";
         int a, b, c;
-        a = b = c = 0;
         System.out.println("Helrld mzf!");
-        System.out.println(1 + abc + 3 + 3+ 3+ 3 +2+2);
+        System.out.println(1 + 3 + 3+ 3+ 3 +2+2);
         System.out.println(a + abc);
     }
 }
