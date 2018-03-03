782b80d6c575954005481fdbfce23f148335546e
Maksim <stelmakh96@gmail.com>
2018-03-03 15:42:01 +0300

    3:41 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        int qwer = 123;
+        System.out.println(qwer);
+        int a,b,c;
+        int d =32432;
+        System.exit(1);
     }
 }
