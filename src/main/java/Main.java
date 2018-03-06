0500e37a3855271428a974e13deef5026d7a1abc
Maksim <stelmakh96@gmail.com>
2018-03-06 16:52:22 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,23 +5,5 @@ public class Main {
         System.out.println("Helrld mzf!");
         System.out.println(1 + "aaa" +3+ 3+ 3 +2+2);
         System.out.println(b + abc);
-        int c = 3;
-        int d = c +1233;
-        int qwer = 123;
-        System.out.println(qwer);
-        int a,b,c;
-        int d =32433;
-        System.exit(1);
-        System.out.println(a + "abc+1"+1+ +abc);
-        System.out.println(1 + "aaa" +3 + 1+ 3 +2+2);
-        System.out.println(a + "abc" +abc);sdasd
-        System.out.println(a + "asc" +abc);
-        System.out.println(a + "dac" +abc);asdsda
-        int d = c +123;
-        System.out.println(a + "dac" +abc);
-        int d = c +123;qweqweqw
-        int f = c +23;
-        int e = a +234asd;asdsadasd
-        int fix = 5;
     }
 }
